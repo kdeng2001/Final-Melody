20767b6a9058bf576796e057b0681b1ec7f921e2
jgy2 <jgy2@njit.edu>
2024-06-17 20:46:29 -0400

    added alley scene

---
+++ Assets/Scripts/joeyScripts/jyj_debug.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static DoorInteractable;
+
+public class jyj_debug : MonoBehaviour
+{
+    [SerializeField] private DoorInteractable door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(door.sceneToEnterName);
+    }
+}
