83000834cb5aa88592d8add4b6761b5357ed211e
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-06-26 14:33:34 -0700

    character/player animation

---
+++ Assets/Scripts/KDScripts/CharacterAnimHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimHandler : MonoBehaviour
+{
+    [SerializeField] private Animator characterAnimator;
+
+    // directions
+    public static string dLeft = "Left";
+    public static string dRight = "Right";
+    public static string dUp = "Up";
+    public static string dDown = "Down";
+    // actions
+    public static string aIdle = "Idle";
+    public static string aWalk = "Walk";
+    public static string aRun = "Run";
+
+    private string currentDirection = "Right";
+    private string currentAction = "Idle";
+    public string currentAnimation = "IdleRight";
+    public void PlayAnimation(string action, string direction)
+    {
+        string newAnimation = action + direction;
+        if(currentAnimation == newAnimation) { return; }
+        Debug.Log("Current animation: " + newAnimation);
+        characterAnimator.Play(newAnimation);
+        currentAnimation = newAnimation;
+        currentDirection = direction;
+        currentAction = action;
+    }
+
+    // disregards current animation check
+    public void SetAnimation(string animation) { characterAnimator.Play(animation); }
+    public void Idle()
+    {
+        //Debug.Log("Idling...");
+        currentAnimation = aIdle + currentDirection;
+        characterAnimator.Play(currentAnimation);
+        currentAction = aIdle;
+    }
+
+}
--- Assets/Scripts/KDScripts/Movement.cs
+++ Assets/Scripts/KDScripts/Movement.cs
@@ -10,6 +10,7 @@ public abstract class Movement : MonoBehaviour
     [SerializeField] private Transform model;
     [SerializeField] private Transform interactor;
     [SerializeField] private int gravity = 10;
+    [SerializeField] private CharacterAnimHandler animHandler;
     private CharacterController _character;
     public Vector2 direction { get; private set; }
     private void Awake()
@@ -33,8 +34,9 @@ public abstract class Movement : MonoBehaviour
         _character.Move(moveValue);
 
         Vector3 lookDir = new Vector3(moveValue.x, 0, moveValue.z).normalized;        
-        if(lookDir == Vector3.zero) { return; }
+        
         Look(lookDir);
+        if (lookDir == Vector3.zero) { return; }
         model.position = transform.position;
         if(interactor == null) { return; }
         interactor.position = transform.position + lookDir.normalized;
@@ -61,9 +63,40 @@ public abstract class Movement : MonoBehaviour
         _character.Move(movePosition);
     }
 
+    int Up = 1;
+    int Down = -1;
+    int Left = 1;
+    int Right = -1;
+    int None = 0;
+    float diagonal = 0.71f;
     public virtual void Look(Vector3 direction)
     {
-        float angle = Vector3.Angle(model.forward, direction);
-        model.eulerAngles = new Vector3(model.eulerAngles.x, model.eulerAngles.y + angle, model.eulerAngles.z);
+        //float angle = Vector3.Angle(model.forward, direction);
+        //model.eulerAngles = new Vector3(model.eulerAngles.x, model.eulerAngles.y + angle, model.eulerAngles.z);
+
+        if(direction == Vector3.zero) { animHandler.Idle(); }
+        else
+        {
+            Debug.Log("direction: " + direction);
+            // left anim --> left / left-down
+            if((direction.x == Left && direction.z == None) || (direction.x > 0 && direction.z < 0)) 
+            {
+                animHandler.PlayAnimation(CharacterAnimHandler.aWalk, CharacterAnimHandler.dLeft);
+            }
+            // up anim --> up / left-up
+            else if((direction.x == None && direction.z == Up) || (direction.x > 0 && direction.z > 0)) {
+                animHandler.PlayAnimation(CharacterAnimHandler.aWalk, CharacterAnimHandler.dUp);
+            }
+            // right anim --> right / right-up
+            else if((direction.x == Right && direction.z == None) || (direction.x < 0 && direction.z > 0))
+            {
+                animHandler.PlayAnimation(CharacterAnimHandler.aWalk, CharacterAnimHandler.dRight);
+            }
+            // down anim --> down / right-down
+            else if ((direction.x == None && direction.z == Down) || (direction.x < 0 && direction.z < 0))
+            {
+                animHandler.PlayAnimation(CharacterAnimHandler.aWalk, CharacterAnimHandler.dDown);
+            }
+        }
     }
 }
