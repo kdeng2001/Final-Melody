a98253412a60a8057b17d1b54cd265bb91a98231
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-06-26 15:03:36 -0700

    place shop keeper sprite, fix wierd rotation

--- Assets/Scripts/KDScripts/SpriteLook.cs
+++ Assets/Scripts/KDScripts/SpriteLook.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class SpriteLook : MonoBehaviour
 {
+    [SerializeField] private bool ignoreY = false;
     private void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        //transform.LookAt(Camera.main.transform);
+
+        Vector3 newtarget = Camera.main.transform.position;
+        if(ignoreY)
+        {
+            newtarget.x = transform.position.x;
+        }
+
+        transform.LookAt(newtarget);
+
     }
 }
