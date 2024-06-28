fd68cf6c42c4481a48e180c6a6f4aa71c4fcb6d8
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-06-28 15:43:09 -0700

    end battle music, return to world music after battle

--- Assets/Scripts/KDScripts/AudioManager.cs
+++ Assets/Scripts/KDScripts/AudioManager.cs
@@ -7,10 +7,14 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
     [SerializeField] private WorldMusicLoops[] worldMusic;
+    [SerializeField] private BattleMusicLoops[] battleMusic;
     //public AK.Wwise.Event[] musicLoops;
     //public string[] sceneName;
-    private uint currentWorldMusicID = 0;
+    private uint currentMusicID = 0;
     private string currentLoopName = "";
+
+    private AK.Wwise.Event currentWorldMusic;
+    private AK.Wwise.Event currentBattleMusic;
     private void Awake()
     {
         if(Instance != null && Instance != this)
@@ -27,22 +31,15 @@ public class AudioManager : MonoBehaviour
     private void OnEnable()
     {
         SceneManager.sceneLoaded += PlaySceneMusic;
+        SceneManager.sceneLoaded += PlayBattleMusic;
+        SceneManager.sceneUnloaded += EndBattleMusic;
     }
     private void OnDisable()
     {
         SceneManager.sceneLoaded -= PlaySceneMusic;
+        SceneManager.sceneLoaded -= PlayBattleMusic;
+        SceneManager.sceneUnloaded -= EndBattleMusic;
     }
-    //public void PlaySceneMusic(Scene scene, LoadSceneMode mode)
-    //{
-    //    for(int i=0; i<musicLoops.Length; i++)
-    //    {
-    //        if(sceneName[i] == scene.name)
-    //        {
-    //            AkSoundEngine.StopPlayingID(currentWorldMusicID);
-    //            currentWorldMusicID = musicLoops[i].Post(gameObject);
-    //        }
-    //    }
-    //}
     public void PlaySceneMusic(Scene scene, LoadSceneMode mode)
     {
         //Debug.Log(scene.name + " mode: " + mode);
@@ -61,12 +58,11 @@ public class AudioManager : MonoBehaviour
                     // otherwise, play new music and stop old one
                     if (loop.musicLoop.Name != currentLoopName)
                     {
-
-                        if (currentWorldMusicID != 0) AkSoundEngine.StopPlayingID(currentWorldMusicID);
+                        if (currentMusicID != 0) AkSoundEngine.StopPlayingID(currentMusicID);
                         Debug.Log("Playing: " + loop.musicLoop.Id);
-                        currentWorldMusicID = loop.musicLoop.Post(Player.Instance.controller.gameObject);
+                        currentMusicID = loop.musicLoop.Post(Player.Instance.controller.gameObject);
                         currentLoopName = loop.musicLoop.Name;
-
+                        currentWorldMusic = loop.musicLoop;
                     }
                     return;
                 }
@@ -74,28 +70,63 @@ public class AudioManager : MonoBehaviour
         }
 
     }
-    public void PlayBattleSceneMusic(Scene scene, LoadSceneMode mode)
+    public void PlayBattleMusic(Scene scene, LoadSceneMode mode)
     {
-        foreach (WorldMusicLoops loop in worldMusic)
+        if(LoadSceneMode.Additive != mode) { return; }
+        foreach (BattleMusicLoops loop in battleMusic)
         {
-            foreach(string sceneName in loop.sceneNames)
+            foreach (string sceneName in loop.sceneNames)
             {
-                if(scene.name == sceneName)
+                // when scene found
+                if (sceneName == scene.name)
                 {
-                    loop.musicLoop.Post(Player.Instance.controller.gameObject);
+                    // if music is unavailable
+                    if (loop.musicLoop == null) { continue; }
+                    // if the current scene's music is same as new scene's music do nothing and return
+                    // otherwise, play new music and stop old one
+                    if (loop.musicLoop.Name != currentLoopName)
+                    {
+                        if( currentMusicID != 0) { AkSoundEngine.StopPlayingID(currentMusicID); }
+                        currentMusicID = loop.musicLoop.Post(Player.Instance.controller.gameObject);
+                        currentLoopName = loop.musicLoop.Name;
+                        currentBattleMusic = loop.musicLoop;
+                    }
+                    return;
                 }
             }
         }
     }
-    public void StopBattleSceneMusic(Scene scene)
+    public void EndBattleMusic(Scene scene)
     {
-
+        foreach (BattleMusicLoops loop in battleMusic)
+        {
+            foreach(string sceneName in loop.sceneNames)
+            {
+                if(sceneName == scene.name)
+                {
+                    if(loop.musicLoop.Name == currentLoopName)
+                    {
+                        if(currentMusicID != 0) { AkSoundEngine.StopPlayingID(currentMusicID); }
+                        currentMusicID = currentWorldMusic.Post(Player.Instance.controller.gameObject);
+                        currentLoopName = currentWorldMusic.Name;
+                        //currentWorldMusic = currentWorldMusic;
+                    }
+                    return;
+                }
+            }
+        }
     }
 }
 
 [System.Serializable]
-
 public struct WorldMusicLoops
+{
+    public string[] sceneNames;
+    public AK.Wwise.Event musicLoop;
+}
+
+[System.Serializable]
+public struct BattleMusicLoops
 {
     public string[] sceneNames;
     public AK.Wwise.Event musicLoop;
