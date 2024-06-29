eeecbdeb4c2879c1ccb087bbaf3f814086c0a935
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-06-29 09:46:22 -0700

    small grammer fix in intro dialogue

--- Assets/Dialogue/Introduction/IntroMomNarration.ink
+++ Assets/Dialogue/Introduction/IntroMomNarration.ink
@@ -5,11 +5,26 @@ INCLUDE ../GLOBALS.ink
 === main ===
 Wake up, {player_name}. #speaker:mum #layout:right #portrait: mom_default
 You can't sleep all day, I have a surprise for your birthday!
-({player_name} gets out of bed and Mom gives {player_name} their instrument of choice.)#layout:default
+({player_name} gets out of bed and Mom gives {player_name} their {instrument_name}.) #layout:default
+
+{instrument_name == "Keytar": -> keytar_guitar}
+{instrument_name == "Guitar": -> keytar_guitar}
+{instrument_name == "Drums": -> drums}
+-> DONE
+=== keytar_guitar ===
 You know when I was a kid, I always played the {instrument_name}. #speaker:mum #layout:right #portrait: mom_default
-You obtained a {instrument_name}!
+You obtained the {instrument_name}!
 It seems oddly similar to the one in that dream of yours. #layout:default
 You tell mom that.
 Oh? Had a dream about this {instrument_name}? #speaker: mum #layout:right #portrait: mom_default
 Well you must really be excited to play then.
 ->END
+
+=== drums ===
+You know when I was a kid, I always played the {instrument_name}. #speaker:mum #layout:right #portrait: mom_default
+You obtained the {instrument_name}!
+It seems oddly similar to the one in that dream of yours. #layout:default
+You tell mom that.
+Oh? Had a dream about these {instrument_name}? #speaker: mum #layout:right #portrait: mom_default
+Well you must really be excited to play then.
+-> END
---
+++ Assets/Scripts/KDScripts/MusicManager.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] public AK.Wwise.Event musicEvent;
+    [SerializeField] BattleMusicState[] battleMusic;
+    [SerializeField] WorldMusicState[] worldMusic;
+
+    private string currentStateName;
+    private string currentStateGroup;
+
+    private WorldMusicState currentWorldMusic;
+
+    public static MusicManager Instance;
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            musicEvent.Post(gameObject);
+            //musicEvent.Post(gameObject);
+            PlaySceneMusic(SceneManager.GetActiveScene(), LoadSceneMode.Single);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += PlaySceneMusic;
+        SceneManager.sceneLoaded += PlayBattleMusic;
+        SceneManager.sceneUnloaded += EndBattleMusic;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= PlaySceneMusic;
+        SceneManager.sceneLoaded -= PlayBattleMusic;
+        SceneManager.sceneUnloaded -= EndBattleMusic;
+    }
+    public void PlaySceneMusic(Scene scene, LoadSceneMode mode)
+    {
+        //Debug.Log(scene.name + " mode: " + mode);
+        //if(LoadSceneMode.Additive == mode) { Debug.Log(scene.name + " mode: " + mode + " will not play"); return; }
+        // loop to find corresponding scene
+        foreach (WorldMusicState state in worldMusic)
+        {
+            foreach (string sceneName in state.sceneNames)
+            {
+                // when scene found
+                if (sceneName == scene.name)
+                {
+                    // if music is unavailable
+                    if (state.stateName == null) { continue; }
+                    // if the current scene's music is same as new scene's music do nothing and return
+                    // otherwise, play new music and stop old one
+                    if (state.stateName != currentStateName)
+                    {
+                        currentStateName = state.stateName;
+                        currentStateGroup = state.stateGroup;
+                        AKRESULT result = AkSoundEngine.SetState(currentStateGroup, currentStateName);
+                        if (result != AKRESULT.AK_Success)
+                        {
+                            Debug.LogError($"SetState failed: {result}");
+                        }
+                        else
+                        {
+                            Debug.Log($"SetState succeeded: {state.stateGroup} -> {state.stateName}");
+                        }
+                        currentWorldMusic = state;
+                        Debug.Log(currentStateGroup + ", " + currentStateName);
+                    }
+                    return;
+                }
+            }
+        }
+
+    }
+    public void PlayBattleMusic(Scene scene, LoadSceneMode mode)
+    {
+        if (LoadSceneMode.Additive != mode) { return; }
+        foreach (BattleMusicState state in battleMusic)
+        {
+            foreach (string sceneName in state.sceneNames)
+            {
+                // when scene found
+                if (sceneName == scene.name)
+                {
+                    // if music is unavailable
+                    if (state.stateName == null) { continue; }
+                    // if the current scene's music is same as new scene's music do nothing and return
+                    // otherwise, play new music and stop old one
+                    if (state.stateName != currentStateName)
+                    {
+                        currentStateName = state.stateName;
+                        currentStateGroup = state.stateGroup;
+                        AKRESULT result = AkSoundEngine.SetState(currentStateGroup, currentStateName);
+                        if (result != AKRESULT.AK_Success)
+                        {
+                            Debug.LogError($"SetState failed: {result}");
+                        }
+                        else
+                        {
+                            Debug.Log($"SetState succeeded: {state.stateGroup} -> {state.stateName}");
+                        }
+                        Debug.Log(currentStateGroup + ", " + currentStateName);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+    public void EndBattleMusic(Scene scene)
+    {
+        foreach (BattleMusicState state in battleMusic)
+        {
+            foreach (string sceneName in state.sceneNames)
+            {
+                if (sceneName == scene.name)
+                {
+                    if (state.stateName == currentStateName)
+                    {
+                        currentStateName = currentWorldMusic.stateName;
+                        currentStateGroup = currentWorldMusic.stateGroup;
+                        AKRESULT result = AkSoundEngine.SetState(currentStateGroup, currentStateName);
+                        if (result != AKRESULT.AK_Success)
+                        {
+                            Debug.LogError($"SetState failed: {result}");
+                        }
+                        else
+                        {
+                            Debug.Log($"SetState succeeded: {state.stateGroup} -> {state.stateName}");
+                        }
+                        Debug.Log(currentStateGroup + ", " + currentStateName);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+}
+
+
+
+[System.Serializable]
+public struct BattleMusicState
+{
+    public string[] sceneNames;
+    public string stateName;
+    public string stateGroup;
+}
+
+
+[System.Serializable]
+public struct WorldMusicState
+{
+    public string[] sceneNames;
+    public string stateName;
+    public string stateGroup;
+}
