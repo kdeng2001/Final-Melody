3d8df49b0a06290904a4c009eb9611fde9612fb3
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-09-20 17:23:36 -0700

    clarify variable naming of property inkVariables + clear commented code

--- Assets/Scripts/KDScripts/Dialogue/DialogueVariables.cs
+++ Assets/Scripts/KDScripts/Dialogue/DialogueVariables.cs
@@ -6,71 +6,52 @@ using System.IO;
 
 public class DialogueVariables : MonoBehaviour, IDataPersistence
 {
-    private Dictionary<string, Ink.Runtime.Object> variables;
-    //private Story globalVariablesStory;
+    private Dictionary<string, Ink.Runtime.Object> inkVariables;
     public void SetUpVariables(TextAsset loadGlobalsJSON)
     {
-        //Debug.Log("Setupvariables");
         // compile GLOBALS story
         Story globalVariablesStory = new Story(loadGlobalsJSON.text);
 
         // initialize dictionary
-        variables = new Dictionary<string, Ink.Runtime.Object>();
+        inkVariables = new Dictionary<string, Ink.Runtime.Object>();
         foreach (string name in globalVariablesStory.variablesState)
         {
             Ink.Runtime.Object value = globalVariablesStory.variablesState.GetVariableWithName(name);
-            variables.Add(name, value);
-            //Debug.Log("Initialize global dialogue variable: " + name + " = " + value);
+            inkVariables.Add(name, value);
         }
     }
 
     public void StartListening(Story story)
     {
         // important that VariablesToStory is called before assigning the listener
-        VariablesToStory(story);
-        story.variablesState.variableChangedEvent += VariableChanged;
+        VarsToStory(story);
+        story.variablesState.variableChangedEvent += inkVarChanged;
     }
     public void StopListening(Story story)
     {
-        story.variablesState.variableChangedEvent -= VariableChanged;
+        story.variablesState.variableChangedEvent -= inkVarChanged;
     }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="name">variable name</param>
-    /// <param name="value">value of variable</param>
-    private void VariableChanged(string name, Ink.Runtime.Object value)
+    private void inkVarChanged(string name, Ink.Runtime.Object value)
     {
-        //Debug.Log("Variable changed: " + name + " = " + value);
-        if(variables.ContainsKey(name))
+        if(inkVariables.ContainsKey(name))
         {
-            variables.Remove(name);
-            variables.Add(name, value);
+            inkVariables.Remove(name);
+            inkVariables.Add(name, value);
         }
     }
-
-    private void VariablesToStory(Story story)
+    private void VarsToStory(Story story)
     {
-        foreach (KeyValuePair<string, Ink.Runtime.Object> variable in variables) 
+        foreach (KeyValuePair<string, Ink.Runtime.Object> var in inkVariables) 
         {
-            //Debug.Log("Variable to story: " + variable.Key + " = " + variable.Value);
-            story.variablesState.SetGlobal(variable.Key, variable.Value);
+            story.variablesState.SetGlobal(var.Key, var.Value);
         }
-
-}
-
+    }
     public void LoadData(GameData data)
     {
         //throw new System.NotImplementedException();
     }
-
     public void SaveData(GameData data)
     {
-        //data.inkVariables.Clear();
-        //foreach(KeyValuePair<string, Ink.Runtime.Object> variable in variables)
-        //{
-        //    data.inkVariables[variable.Key] = variable.Value;
-        //}
+        //throw new System.NotImplementedException();
     }
 }
